2015e3ef48705e16479b1fe6e6732ab332d9e526
ygalkin <y.galkin@gmail.com>
2021-04-01 10:02:52 -0400

    701. Insert into a Binary Search Tree

---
+++ app/leetcode/insert-into-a-binary-search-tree.h
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/insert-into-a-binary-search-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* findNode(TreeNode* parent, int val) const {
+        auto child = (val < parent->val) ? parent->left : parent->right;
+        if (child == nullptr) {
+            return parent;
+        }
+
+        return findNode(child, val);
+    }    
+    
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (root == nullptr) {
+            return new TreeNode(val);
+        }
+
+        auto parent = findNode(root, val);
+        auto child = (val < parent->val) ? &parent->left : &parent->right;
+        *child = new TreeNode(val);
+        return root;
+    }
+};
