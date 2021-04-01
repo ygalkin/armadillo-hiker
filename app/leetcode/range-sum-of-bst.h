2de6c69fb3ea0b4263cc97967b489034dc8766c1
ygalkin <y.galkin@gmail.com>
2021-04-01 13:42:51 -0400

    938. Range Sum of BST

---
+++ app/leetcode/range-sum-of-bst.h
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/range-sum-of-bst/
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (root == nullptr)
+            return 0;
+        
+        auto sum{0};
+        
+        if (low < root->val) 
+            sum += rangeSumBST(root->left, low, high);
+        
+        if (high > root->val) 
+            sum += rangeSumBST(root->right, low, high);
+
+        if (root->val >= low && root->val <= high)
+            sum += root->val;
+        
+        return sum;        
+    }
+};
