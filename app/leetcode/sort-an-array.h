a64e051335a4e7639976e3b72bbbcb5fea4b874d
ygalkin <y.galkin@gmail.com>
2021-04-03 20:15:26 -0400

    update

--- app/leetcode/sort-an-array.h
+++ app/leetcode/sort-an-array.h
@@ -11,3 +11,26 @@ public:
         return nums;
     }
 };
+
+// Quick sort
+class Solution {
+public:
+    template< class IterT >
+    void quick_sort(IterT first, IterT last) {
+        if (first == last)
+            return;
+
+        // Partitioning
+        const auto pivot = *(last - 1); // pivot element is a last element
+        auto bound = std::partition(first, last, [&pivot](const auto& i) { return i < pivot; });
+        std::iter_swap(bound, last - 1);
+
+        quick_sort(first, bound);
+        quick_sort(bound + 1, last);
+    }
+    
+    vector<int> sortArray(vector<int>& nums) {
+        quick_sort(std::begin(nums), std::end(nums));
+        return nums;
+    }
+};
