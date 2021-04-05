c371233bd979692f7fb719dede86c7c2d4763012
ygalkin <y.galkin@gmail.com>
2021-04-04 20:55:29 -0400

    update

--- app/leetcode/sort-an-array.h
+++ app/leetcode/sort-an-array.h
@@ -20,13 +20,35 @@ public:
         if (first == last)
             return;
 
-        // Partitioning
+        // Partitioning. Lomuto’s partition scheme
         const auto pivot = *(last - 1); // pivot element is a last element
         auto bound = std::partition(first, last, [&pivot](const auto& i) { return i < pivot; });
         std::iter_swap(bound, last - 1);
 
         quick_sort(first, bound);
         quick_sort(bound + 1, last);
+    }
+    
+    vector<int> sortArray(vector<int>& nums) {
+        quick_sort(std::begin(nums), std::end(nums));
+        return nums;
+    }
+};
+
+class Solution {
+public:
+    template< class IterT >
+    void quick_sort(IterT first, IterT last) {
+        if (first == last)
+            return;
+
+        // Partitioning. Hoare partition scheme
+        const auto pivot = *(first + (std::distance(first, last) / 2));
+        auto bound1 = std::partition(first, last, [&pivot](const auto& i) { return i < pivot; });
+        auto bound2 = std::partition(bound1, last, [&pivot](const auto& i) { return !(i > pivot); });
+
+        quick_sort(first, bound1);
+        quick_sort(bound2, last);
     }
     
     vector<int> sortArray(vector<int>& nums) {
