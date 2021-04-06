35906f6f5b2b5a6df0f4ec75e56467160e8572f2
ygalkin <y.galkin@gmail.com>
2021-04-06 12:35:56 -0400

    Tree: Level Order Traversal

---
+++ app/hackerrank/data-structures/tree-level-order-traversal.h
@@ -0,0 +1,31 @@
+// https://www.hackerrank.com/challenges/tree-level-order-traversal/problem
+
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+
+     void levelOrder(Node * root) {
+        if (root == nullptr) {
+            return;
+        }
+        
+        std::queue<Node*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            auto node = q.front();
+            q.pop();
+            std::cout << node->data << " ";
+            if (node->left) q.push(node->left);
+            if (node->right) q.push(node->right);
+        }
